056792beb922a22b458577cbfe5f3b42014464ca
caoliqiang <caoliqiang@139.com>
2021-01-30 16:34:15 +0800

    2021年1月30日第一次作业 参数化与数据去懂你

---
+++ pythoncode/Calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 被测类：计算器
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a ,b):
+        return a - b
+
+    def multi(self,a, b):
+        return a * b
+
+    def div(self, a,b):
+        return (a / b)
+
+
+    # def div(self, a, b,result):
+    #     # return a / b
+    #     assert result == a / b
+
